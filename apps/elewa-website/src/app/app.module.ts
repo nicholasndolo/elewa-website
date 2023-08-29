1b6e3eda64b373838239aec65aa664e1390ee318
Nicholas <nickndolo95@gmail.com>
2023-08-29 15:46:11 +0300

    :sparkles: feat: bind a click event with a handleClick method in the elewa-normal-button-component

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app.routing';
     AppRoutingModule,
     HttpClientModule,
     TranslocoRootModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent],
--- libs/elements/layout/buttons/src/lib/components/elewa-normal-button/elewa-normal-button.component.ts
+++ libs/elements/layout/buttons/src/lib/components/elewa-normal-button/elewa-normal-button.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { ButtonData } from '@elewa-website/models/schema/ui/buttons';
 
 @Component({
   selector: 'elewa-website-elewa-normal-button',
   templateUrl: './elewa-normal-button.component.html',
   styleUrls: ['./elewa-normal-button.component.scss'],
 })
-export class ElewaNormalButtonComponent {}
+export class ElewaNormalButtonComponent {
+  
+  @Output() click: EventEmitter<void> = new EventEmitter<void>()
+
+  handleClick(): void{
+    //emit click event
+    this.click.emit()
+  }
+}
